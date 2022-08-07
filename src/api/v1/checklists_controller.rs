5497b9a62713cc92e92581d063c4d00ced9237aa
Eric Grimsborn <eric.o.berglund@gmail.com>
2022-08-07 12:54:04 +0200

    use uuid in path parameters instead of manually checking

--- src/api/v1/checklists_controller.rs
+++ src/api/v1/checklists_controller.rs
@@ -1,7 +1,9 @@
 use crate::{
     api::{
         auth::user_guard::AuthenticatedUser,
-        helpers::{parse_uuid, ApiResultFrom, ArgentApiResult, ArgentResult, NewData, OkData},
+        helpers::{
+            convert_uuid, parse_uuid, ApiResultFrom, ArgentApiResult, ArgentResult, NewData, OkData,
+        },
     },
     data::{
         checklists::{
@@ -15,7 +17,7 @@ use crate::{
     },
     error::{ArgentError, SimpleMessage},
 };
-use rocket::{delete, get, http::Status, post, routes, serde::json::Json, Route};
+use rocket::{delete, get, http::Status, post, routes, serde, serde::json::Json, Route};
 use uuid::Uuid;
 
 #[get("/checklists")]
@@ -43,12 +45,15 @@ async fn create_checklist(
 #[get("/checklists/<id>/items")]
 async fn get_checklist_items(
     mut checklists_store: ChecklistStore,
-    id: String,
+    id: serde::uuid::Uuid,
     user: AuthenticatedUser,
 ) -> ArgentApiResult<Vec<ChecklistItem>> {
-    let id = parse_uuid(&id, Status::NotFound)?;
-    check_access(&mut checklists_store, id, user.get()).await?;
-    checklists_store.get_checklist_items(id).await.api()
+    let checklist_id = convert_uuid(&id);
+    check_access(&mut checklists_store, checklist_id, user.get()).await?;
+    checklists_store
+        .get_checklist_items(checklist_id)
+        .await
+        .api()
 }
 
 #[post("/checklistitems", data = "<checklistitem_request>")]
@@ -66,68 +71,71 @@ async fn create_checklistitem(
 #[post("/checklistitems/<id>/done")]
 async fn set_item_done(
     mut checklists_store: ChecklistStore,
-    id: String,
+    id: serde::uuid::Uuid,
     _user: AuthenticatedUser,
 ) -> ArgentApiResult<SimpleMessage> {
-    let id = parse_uuid(&id, Status::NotFound)?;
-    checklists_store.set_item_done(id, true).await?;
+    let item_id = convert_uuid(&id);
+    checklists_store.set_item_done(item_id, true).await?;
     ArgentApiResult::new_ok()
 }
 
 #[post("/checklistitems/<id>/not-done")]
 async fn set_item_not_done(
     mut checklists_store: ChecklistStore,
-    id: String,
+    id: serde::uuid::Uuid,
     _user: AuthenticatedUser,
 ) -> ArgentApiResult<SimpleMessage> {
-    let id = parse_uuid(&id, Status::NotFound)?;
-    checklists_store.set_item_done(id, false).await?;
+    let item_id = convert_uuid(&id);
+    checklists_store.set_item_done(item_id, false).await?;
     ArgentApiResult::new_ok()
 }
 
 #[delete("/checklists/<id>")]
 async fn delete_checklist(
     mut checklists_store: ChecklistStore,
-    id: String,
+    id: serde::uuid::Uuid,
     user: AuthenticatedUser,
 ) -> ArgentApiResult<SimpleMessage> {
-    let id = parse_uuid(&id, Status::NotFound)?;
-    check_owner(&mut checklists_store, id, user.get()).await?;
-    checklists_store.delete_checklist(id).await?;
+    let checklist_id = convert_uuid(&id);
+    check_owner(&mut checklists_store, checklist_id, user.get()).await?;
+    checklists_store.delete_checklist(checklist_id).await?;
     ArgentApiResult::new_ok()
 }
 
 #[get("/checklists/<id>")]
 async fn get_checklist(
     mut checklists_store: ChecklistStore,
-    id: String,
+    id: serde::uuid::Uuid,
     user: AuthenticatedUser,
 ) -> ArgentApiResult<Checklist> {
-    let id = parse_uuid(&id, Status::NotFound)?;
-    check_access(&mut checklists_store, id, user.get()).await?;
-    checklists_store.get_checklist_by_id(id).await.api()
+    let checklist_id = convert_uuid(&id);
+    check_access(&mut checklists_store, checklist_id, user.get()).await?;
+    checklists_store
+        .get_checklist_by_id(checklist_id)
+        .await
+        .api()
 }
 
 #[post("/checklists/<id>/clear-done")]
 async fn clear_done(
     mut checklists_store: ChecklistStore,
-    id: String,
+    id: serde::uuid::Uuid,
     user: AuthenticatedUser,
 ) -> ArgentApiResult<SimpleMessage> {
-    let id = parse_uuid(&id, Status::NotFound)?;
-    check_access(&mut checklists_store, id, user.get()).await?;
-    checklists_store.clear_done(id).await?;
+    let checklist_id = convert_uuid(&id);
+    check_access(&mut checklists_store, checklist_id, user.get()).await?;
+    checklists_store.clear_done(checklist_id).await?;
     ArgentApiResult::new_ok()
 }
 
 #[post("/checklists/<id>/share", data = "<share_req>")]
 async fn share(
     mut checklists_store: ChecklistStore,
-    id: String,
+    id: serde::uuid::Uuid,
     user: AuthenticatedUser,
     share_req: Json<ShareRequest>,
 ) -> ArgentApiResult<SimpleMessage> {
-    let checklist_id = parse_uuid(&id, Status::NotFound)?;
+    let checklist_id = convert_uuid(&id);
     let share_req = share_req.into_inner();
     let user_id = parse_uuid(&share_req.user_id, Status::BadRequest)?;
     check_owner(&mut checklists_store, checklist_id, user.get()).await?;
@@ -140,12 +148,12 @@ async fn share(
 #[post("/checklists/<id>/unshare/<user_id>")]
 async fn un_share(
     mut checklists_store: ChecklistStore,
-    id: String,
-    user_id: String,
+    id: serde::uuid::Uuid,
+    user_id: serde::uuid::Uuid,
     user: AuthenticatedUser,
 ) -> ArgentApiResult<SimpleMessage> {
-    let checklist_id = parse_uuid(&id, Status::NotFound)?;
-    let user_id = parse_uuid(&user_id, Status::NotFound)?;
+    let checklist_id = convert_uuid(&id);
+    let user_id = convert_uuid(&user_id);
     check_owner(&mut checklists_store, checklist_id, user.get()).await?;
     let users = checklists_store
         .get_users_access_for_checklist(checklist_id)
@@ -159,11 +167,6 @@ async fn un_share(
             "cannot remove the last owner of a checklist",
         ));
     }
-    //     val checklistOwners = userDataStore.getUsersForChecklist(checklistId)
-    //     .filter { it.checklistAccessType == ChecklistAccessType.Owner }
-    // if (checklistOwners.size == 1 && checklistOwners.first().id == userId) {
-    //     throw BadRequestException("Cannot remove last owner of a list")
-    // }
     checklists_store
         .remove_useraccess(checklist_id, user_id)
         .await?;
@@ -173,10 +176,10 @@ async fn un_share(
 #[get("/checklists/<id>/users")]
 async fn get_users_for_checklist(
     mut checklists_store: ChecklistStore,
-    id: String,
+    id: serde::uuid::Uuid,
     user: AuthenticatedUser,
 ) -> ArgentApiResult<Vec<UserAccess>> {
-    let checklist_id = parse_uuid(&id, Status::NotFound)?;
+    let checklist_id = convert_uuid(&id);
     check_access(&mut checklists_store, checklist_id, user.get()).await?;
     let user_accesses = checklists_store
         .get_users_access_for_checklist(checklist_id)
